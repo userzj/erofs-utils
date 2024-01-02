38d61268b90716d6e98e09bdce5cde9bb656ac9a
Jia Zhu <zhujia.zj@bytedance.com>
2024-01-02 11:20:37 +0800

    erofs-utils: tests: modify fscache/006
    With the introduction of recover mechanism in cachefiles module of Linux
    kernel, a closed anonymous fd will no longer cause the request to return
    -EIO. Instead, requests will be held and wait for a 'restore' cmd from
    user daemon to reopen the fd and reprocess them.
    
    This commit tests recover mechanism of cachefiles with a specialized
    user daemon.
    
    Signed-off-by: Jia Zhu <zhujia.zj@bytedance.com>

--- tests/src/fscache006.c
+++ tests/src/fscache006.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,6 +10,10 @@
 #include <stdint.h>
 #include <limits.h>
 #include <errno.h>
+#include <sys/prctl.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "internal.h"
 
@@ -36,15 +41,6 @@ static void parse_options(int argc, char *argv[])
 	}
 }
 
-/* error injection - close anon_fd prematurely */
-static void close_anon_fd_premaure(struct cachefiles_msg *msg)
-{
-	struct cachefiles_open *load;
-
-	load = (void *)msg->data;
-	close(load->fd);
-}
-
 static int process_one_req(int devfd)
 {
 	char buf[CACHEFILES_MSG_MAX_SIZE];
@@ -70,13 +66,11 @@ static int process_one_req(int devfd)
 
 	switch (msg->opcode) {
 	case CACHEFILES_OP_OPEN:
-		ret = process_open_req(devfd, msg);
-		if (!ret)
-			close_anon_fd_premaure(msg);
-		return ret;
+		return process_open_req(devfd, msg);
 	case CACHEFILES_OP_CLOSE:
 		return process_close_req(devfd, msg);
 	case CACHEFILES_OP_READ:
+		sleep(1);
 		return process_read_req(devfd, msg);
 	default:
 		printf("invalid opcode %d\n", msg->opcode);
@@ -84,18 +78,12 @@ static int process_one_req(int devfd)
 	}
 }
 
-int main(int argc, char *argv[])
+static int handle_requests(int devfd)
 {
+	int ret = 0;
 	struct pollfd pollfd;
-	int fd, ret;
 
-	parse_options(argc, argv);
-
-	fd = daemon_get_devfd(fscachedir, tag);
-	if (fd < 0)
-		return -1;
-
-	pollfd.fd = fd;
+	pollfd.fd = devfd;
 	pollfd.events = POLLIN;
 
 	while (1) {
@@ -111,8 +99,53 @@ int main(int argc, char *argv[])
 		}
 
 		/* process all pending read requests */
-		while (!process_one_req(fd)) {}
+		while (!process_one_req(devfd)) {}
 	}
 
 	return 0;
+}
+
+static int startup_child_process(int devfd)
+{
+	pid_t pid;
+	int wstatus = 0;
+
+restart:
+	pid = fork();
+	if (pid == 0) {
+		/* child process */
+		printf("child: ready to handle requests\n");
+		prctl(PR_SET_NAME, "cache-child",NULL,NULL,NULL);
+		handle_requests(devfd);
+	} else if (pid > 0)  {
+		/* parent process */
+		printf("supervisor[pid %d]: fork child process[pid %d]\n", getpid(), pid);
+		if (pid == wait(&wstatus)) {
+			if (WIFSIGNALED(wstatus)) {
+				ssize_t written = write(devfd, "restore", 7);
+				if (written != 7) {
+					printf("supervisor: write restore cmd recover failed\n");
+					return -1;
+				}
+				printf("supervisor: restore in-flight IO success, restart child process...\n");
+				goto restart;
+			}
+		}
+	}
+
+	return pid;
+}
+
+int main(int argc, char *argv[])
+{
+	struct pollfd pollfd;
+	int fd, ret;
+
+	parse_options(argc, argv);
+
+	fd = daemon_get_devfd(fscachedir, tag);
+	if (fd < 0)
+		return -1;
+
+	return startup_child_process(fd);
 }
